e76dfebe87a32573b43acf1356809e625c600157
Josh <2107767@talnet.nl>
2019-11-20 10:14:46 +0100

    Uitwerking van de exercise

---
+++ for_klas.php
@@ -0,0 +1,7 @@
+<?php
+
+$name = array(0 => "Sjonnie de Wiel", 1 => "Herman Kaal", 2 => "Henk de Steen", 3 => "Inge Kerkhoven", 4 => "Gert kruiswijk");
+
+for ($i = 0; $i < count($name); $i++) {
+	echo $name[$i] . PHP_EOL;
+}
